2a8559412e1f503bd39d5a97e0f3825cc7bf59a2
mao <1296193245@qq.com>
2023-12-08 17:36:25 +0800

    uniapp-convas-components

---
+++ pages/index/test2/test2.vue
@@ -0,0 +1,25 @@
+<template>
+	<view>
+		<web-view src="https://www.bilibili.com/" @loaded="loadWeb"></web-view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			loadWeb(e)
+			{
+				console.log('加载完成',e);
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
